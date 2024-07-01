d7bd2281128e88de513d12b78974b57698c556ef
oneday <oneday@onedaydeMacBook-Air.local>
2024-07-02 01:06:36 +0800

    ai_quant_react

---
+++ constants/navigation.tsx
@@ -0,0 +1,44 @@
+export const navigation = [
+    {
+        id: "0",
+        title: "数据看版",
+        icon: "home",
+        url: "/",
+    },
+    {
+        id: "1",
+        title: "资产中心",
+        icon: "wallet",
+        url: "/my-assets",
+    },
+    {
+        id: "2",
+        title: "交易信号",
+        icon: "notification",
+        url: "/notification",
+    },
+    {
+        id: "3",
+        title: "交易记录",
+        icon: "trade",
+        url: "/trade",
+    },
+    {
+        id: "4",
+        title: "智能量化",
+        icon: "star-plus",
+        url: "/chat",
+    },
+    {
+        id: "5",
+        title: "付费计划",
+        icon: "star",
+        url: "/pricing",
+    },
+    {
+        id: "6",
+        title: "用户设置",
+        icon: "settings",
+        url: "/settings",
+    },
+];
